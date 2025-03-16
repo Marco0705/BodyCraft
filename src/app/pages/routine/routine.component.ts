98fee72e4c44fc64ff2b19d4adb831c715893681
Marco0705 <marco070593@gmail.com>
2025-03-16 20:04:52 +0100

    First commit

---
+++ src/app/pages/routine/routine.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-routine',
+  imports: [],
+  templateUrl: './routine.component.html',
+  styleUrl: './routine.component.css'
+})
+export class RoutineComponent {
+
+}
