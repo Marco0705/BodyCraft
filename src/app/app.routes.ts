5b94472bb2336d1a95769890b058ddc826c4d5c6
victorhponce <vhp06dev@gmail.com>
2025-03-23 12:03:01 +0100

    testing on router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { DietComponent } from './pages/diet/diet.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'home', pathMatch: 'full',
+    },
+    {
+        path: "dieta", component: DietComponent
+    }
+];
