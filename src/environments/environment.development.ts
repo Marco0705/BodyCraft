b1cd2015963ac3181478b68ef78cd577958b1e12
bdjoseluis <josbotdev@gmail.com>
2025-05-18 22:18:45 +0200

    truco

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://bodycraft.josbotdev.es',
-  //apiUrl: 'http://localhost:5000',
+  //apiUrl: 'https://bodycraft.josbotdev.es',
+  apiUrl: 'http://localhost:5000',
   apiKey: '',
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://bodycraft.josbotdev.es',
-  //apiUrl: 'localhost:5000',
+  //apiUrl: 'https://bodycraft.josbotdev.es',
+  apiUrl: 'localhost:5000',
   apiKey: '',
 };
